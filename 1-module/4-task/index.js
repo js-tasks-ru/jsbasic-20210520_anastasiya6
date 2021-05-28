4fe5fbec18a85c1b44524039205de1b723801bb9
Shulenina Anastasii <553shulshul@gmail.com>
2021-05-29 00:02:01 +0300

    done 2-5 task module 2

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
-function checkSpam(str) {
-  // ваш код...
-}
+  function checkSpam(str){
+  	if(str.search(/1xBet/i)>=0 || str.search(/XXX/i)>=0){
+  		return true
+  	}
+  	 else {
+  	 	return false
+  	 }
+  	}
+
